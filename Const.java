62508e6275fb0ba30f39ae948a2007217bb4f1ec
VarshiniManokaran <varshini6678@gmail.com>
2024-08-07 13:27:35 +0530

    Add files via upload

---
+++ Const.java
@@ -0,0 +1,14 @@
+public class Const {
+public static final String name="Varshini";
+public static final double weight=41.5;
+public static final  int age=30;
+
+public static void main(String[] args) {
+    System.out.println("The Name is:"+name);
+    System.out.println("The weight is:"+weight);
+    System.out.println("The  age is:"+age);
+    
+        
+    }
+    
+}
